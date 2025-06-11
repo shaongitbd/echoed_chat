04accc57a92843bf9a004e2b612aa10729916b2b
Siam <shaongit@gmail.com>
2025-06-11 17:24:54 +0600

    removed duplicate sidebar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -14,19 +14,7 @@ const Layout = () => {
   return (
     <div className="flex h-screen bg-white">
       {/* Sidebar - hidden on mobile unless toggled */}
-      <div className={`fixed inset-y-0 left-0 z-30 w-64 transform bg-white border-r transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:w-64 ${
-        sidebarOpen ? 'translate-x-0' : '-translate-x-full'
-      }`}>
-        <Sidebar />
-      </div>
-      
-      {/* Mobile overlay */}
-      {sidebarOpen && (
-        <div 
-          className="fixed inset-0 z-20 bg-black bg-opacity-50 lg:hidden"
-          onClick={toggleSidebar}
-        />
-      )}
+    
       
       {/* Main content */}
       <div className="flex flex-col flex-1 overflow-hidden">
